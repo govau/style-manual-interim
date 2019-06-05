c0e8f97ca9b17d120b51e22269f16df22e29888b
Michael Tran <michael@dublue.com>
2019-06-05 15:03:25 +1000

    Added timeline, adjust templating and styling

--- _assets/scripts/main.js
+++ _assets/scripts/main.js
@@ -1,2 +1,13 @@
 //= require designsystem.min.js
+//= require timeline.js
 
+(function() {
+
+    var el = document.getElementById('timeline');
+    if (typeof(el) != 'undefined' && el != null) {
+        var timeline = new TL.Timeline('timeline', 'assets/timeline.json', {
+            scale_factor: 1
+        });
+    }
+
+})();
