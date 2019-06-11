1d20754a10af2fc02dbcac3231b3b7ab60d2c7c1
Michael Tran <michael@dublue.com>
2019-06-11 11:53:40 +1000

    Produce history page with the same timeline.json data so it doesn't need to change

--- _assets/scripts/main.js
+++ _assets/scripts/main.js
@@ -10,4 +10,22 @@
         });
     }
 
+    var el = document.getElementById('non-interactive-timeline');
+    if (typeof(el) != 'undefined' && el != null) {
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.onreadystatechange = function() {
+          if (this.readyState == 4 && this.status == 200) {
+            var tl = JSON.parse(this.responseText);
+            var html = '';
+            for ( i = 0; i < tl.events.length; i++ ) {
+                html += '<h2>' + tl.events[i].start_date.year + ' - ' + tl.events[i].text.headline + '</h2>' +
+                    tl.events[i].text.text;
+            }
+            el.innerHTML = html;
+          }
+        };
+        xmlhttp.open('GET', 'assets/timeline.json', true);
+        xmlhttp.send();
+    }
+
 })();
